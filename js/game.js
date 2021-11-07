3959372b568540b3646003591f356ab2824e8fb0
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-11-07 12:02:17 +0200

    add field

--- js/game.js
+++ js/game.js
@@ -1,3 +1,22 @@
 const field = document.querySelector(".field");
 
-for (let i = 1; i <= 15; i++) {}
+const empty = {
+  top: 0,
+  left: 0,
+};
+
+for (let i = 1; i <= 15; i++) {
+  const cell = document.createElement("div");
+  cell.className = "cell";
+  cell.innerHTML = i;
+
+  const left = i % 4;
+  const top = (i - left) / 4;
+  cell.style.left = `${left * cellSize}px`;
+  cell.style.top = `${top * cellSize}px`;
+  field.append(cell);
+  cell.addEventListener("click", () => {
+    cell.style.left = `${empty.left * cellSize}px`;
+    cell.style.top = `${empty.top * cellSize}px`;
+  });
+}
