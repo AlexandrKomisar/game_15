b6e37776497b8b701dd4b068efc5011bd17b4fea
AlexandrKomisar <82936052+AlexandrKomisar@users.noreply.github.com>
2021-11-07 17:10:28 +0200

    v0.01

--- js/game.js
+++ js/game.js
@@ -1,6 +1,8 @@
 const field = document.querySelector(".field");
+const cellSize = 100;
 
 const empty = {
+  value: 0,
   top: 0,
   left: 0,
 };
@@ -26,27 +28,39 @@ function move(index) {
   empty.top = cell.top;
   cell.left = emptyLeft;
   cell.top = emptyTop;
+
+  const isFinished = cells.every((cell) => {
+    return cell.value === cell.top * 4 + cell.left;
+  });
+
+  if (isFinished) {
+      alert('You won!')
+  }
 }
+
+const numbers = [...Array(15).keys()].sort(() => Math.random() - 0.5);
 
 for (let i = 1; i <= 15; i++) {
   const cell = document.createElement("div");
+  const value = numbers[i - 1] + 1;
   cell.className = "cell";
-  cell.innerHTML = i;
+  cell.innerHTML = value;
 
   const left = i % 4;
   const top = (i - left) / 4;
 
-  cell.push({
+  cells.push({
     left: left,
     top: top,
-    element: cell,
+    element: cell
   });
 
   cell.style.left = `${left * cellSize}px`;
   cell.style.top = `${top * cellSize}px`;
+
   field.append(cell);
+
   cell.addEventListener("click", () => {
-    cell.style.left = `${empty.left * cellSize}px`;
-    cell.style.top = `${empty.top * cellSize}px`;
+    move(i);
   });
 }
